ca52ccf7f490d8ade549e45c996903434905d749
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-09-04 23:53:06 +0530

    add fps limit for game

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init() # initalizing pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) # set display size for screen
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +20,9 @@ def main():
         
         screen.fill('Black')
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
